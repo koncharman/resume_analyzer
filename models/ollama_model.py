dcb9a09133c62635735afb219837b02e40606433
zapfl <zapflyer@hotmail.com>
2026-08-04 16:04:13 +0300

    test olama

---
+++ models/ollama_model.py
@@ -0,0 +1,15 @@
+import ollama
+
+
+def ask_ollama(prompt):
+    response = ollama.chat(
+        model="llama3.2:3b",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response["message"]["content"]
