f306d50fff99686e92569c2bb909721f0a66fd2f
zapfl <zapflyer@hotmail.com>
2026-08-14 15:28:23 +0300

    test olama

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,21 +1,57 @@
 
 import pandas as pd
+import ezodf
 
 text_cols=['conceptUri','preferredLabel','altLabels','description']
 
 def load_skills():
-    df = pd.read_excel(
-        "data/skills_en.ods",
-        engine="odf"
+
+    doc = ezodf.opendoc("data/skills_en.ods")
+    sheet = doc.sheets[0]
+
+    data = [
+        [cell.value for cell in row]
+        for row in sheet.rows()
+    ]
+
+    df=pd.DataFrame(
+        data[1:],
+        columns=data[0]
     )
+
+    df["altLabels"] = (
+        df["altLabels"]
+        .fillna("")
+        .str.replace("\r\n", "; ", regex=False)
+        .str.replace("\n", "; ", regex=False)
+        .str.replace("\r", "; ", regex=False)
+    )
+
 
     return df.loc[:,text_cols]
 
 
 def load_occupations():
-    df = pd.read_excel(
-        "data/occupations_en.ods",
-        engine="odf"
+
+    doc = ezodf.opendoc("data/occupations_en.ods")
+    sheet = doc.sheets[0]
+
+    data = [
+        [cell.value for cell in row]
+        for row in sheet.rows()
+    ]
+
+    df=pd.DataFrame(
+        data[1:],
+        columns=data[0]
+    )
+
+    df["altLabels"] = (
+        df["altLabels"]
+        .fillna("")
+        .str.replace("\r\n", "; ", regex=False)
+        .str.replace("\n", "; ", regex=False)
+        .str.replace("\r", "; ", regex=False)
     )
 
     return df.loc[:,text_cols]
